0bf5c21d6e1550f542467daa990c02d48b28515b
“Artem <artemsoroka.reverbed@gmail.com>
2026-07-08 22:32:36 +0300

    All ui classes + slots and methods

--- Practice-Task-v1/mainwindow.h
+++ Practice-Task-v1/mainwindow.h
@@ -2,22 +2,55 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTableWidget>
+#include <QListWidget>
+#include <QListView>
+#include <QPushButton>
+#include <QStringListModel>
+#include <QKeyEvent>
+#include <QLabel>
+#include <QLineEdit>
+#include "bus_logic.h"
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+
+private slots:
+    void addBus();
+    void removeBus();
+    void loadFromFile();
+    void sortHoareAsc();
+    void sortHoareDesc();
+    void showContextMenu(const QPoint &pos);
+    void applyMaxSize();
+
 private:
-    Ui::MainWindow *ui;
+    void updateViews();
+
+    QList<BusTrip> busList;
+    int maxArraySize;
+
+    QTableWidget *tableWidget;
+    QListWidget *listWidget;
+    QListView *listView;
+    QStringListModel *listViewModel;
+
+    QLabel *lblSize;
+    QLineEdit *leMaxSize;
+    QPushButton *btnApplySize;
+
+    QPushButton *btnAdd;
+    QPushButton *btnRemove;
+    QPushButton *btnLoad;
+    QPushButton *btnSortHoareAsc;
+    QPushButton *btnSortHoareDesc;
 };
-#endif // MAINWINDOW_H
+
+#endif
